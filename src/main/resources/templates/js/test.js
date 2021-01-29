3b1d0506f93a5770154a58aec6e68028936c3973
koeonyack <covenant.ko@kakao.com>
2021-01-29 19:50:57 +0900

    make it better

---
+++ src/main/resources/templates/js/test.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    let element = document.getElementsByClassName("vue-terminal-input")[0];
+    while (document.activeElement.id != element.id) {
+        element.focus();
+    }
+};
